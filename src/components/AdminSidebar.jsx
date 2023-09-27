19ef2c796dc27d2955e7fafaff11c892cdbd4653
santiago centeno <santiagocentenot@gmail.com>
2023-09-26 21:26:03 -0400

    corrigiendo funcionalidades de roles

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 export default function AdminSidebar() {
-  const { logout } = useAuth({
+  const { logout, user } = useAuth({
     middleware: "auth",
   });
+
+  const navigate = useNavigate();
+
+  const onRedirectTo = () => {
+    if (user?.admin === 1) {
+      navigate("/admin/productos");
+    } else {
+      navigate("/");
+    }
+  };
+
   return (
     <aside className="md:w-72">
-      <div className="p-4">
+      <div
+        className="p-4 flex items-center justify-center"
+        onClick={() => onRedirectTo()}
+      >
         <img src="/img/logo.svg" alt="" className="w-40" />
       </div>
       <nav className="flex flex-col p-4 text-center gap-4">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,16 +3,34 @@ import React from "react";
 import Categoria from "./Categoria";
 import useQuiosco from "../hooks/useQuiosco";
 import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
   const { categorias } = useQuiosco();
   const { logout, user } = useAuth({
     middleware: "auth",
   });
+
+  const navigate = useNavigate();
+
+  const onRedirectTo = () => {
+    if (user?.admin === 1) {
+      navigate("/admin/productos");
+    } else {
+      navigate("/");
+    }
+  };
+
+  console.log(user);
   return (
     <aside className="w-72">
-      <div className="p-4">
-        <img src="img/logo.svg" className="w-40" alt="" />
+      <div className="p-4 flex items-center justify-center">
+        <img
+          src="img/logo.svg"
+          className="w-40"
+          alt=""
+          onClick={() => onRedirectTo()}
+        />
       </div>
       <p className="my-4 text-xl text-center">Hola: {user?.name} </p>
       <div className="mt-10">
